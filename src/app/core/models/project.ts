cb08666365abaafd0bb93ec029610251f638f93e
jaydeep pawar <jdpppawar@gmail.com>
2022-06-06 10:24:33 +0530

    portfolio implementation commit

---
+++ src/app/core/models/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+
+    name: string;
+    stack: { iconClasses: string, name: string };
+    description: string;
+    sourceUrl: string;
+    previewUrl: string;
+    featured?: boolean;
+}
---
+++ src/app/portfolio/portfolio.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PortfolioRoutingModule } from './portfolio-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    PortfolioRoutingModule
+  ]
+})
+export class PortfolioModule { }
